761fb7cd1bbdbad65ff6ba24ac3365cf47dbc614
Juan <j.juancamilojurado@gmail.com>
2019-09-15 12:20:47 -0500

    Creacion del programa

---
+++ Final2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Final2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txthoras = new System.Windows.Forms.TextBox();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese las horas trabajadas:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ingrese el valor de las horas:";
+            // 
+            // txthoras
+            // 
+            this.txthoras.Location = new System.Drawing.Point(210, 89);
+            this.txthoras.Name = "txthoras";
+            this.txthoras.Size = new System.Drawing.Size(100, 20);
+            this.txthoras.TabIndex = 4;
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(210, 163);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor.TabIndex = 5;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(248, 224);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(81, 23);
+            this.btncalcular.TabIndex = 6;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Su salario es:";
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(112, 224);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(100, 20);
+            this.txttotal.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(45, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 22);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Salario de horas trabajadas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 276);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.txthoras);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txthoras;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Final2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double hor = double.Parse(txthoras.Text);
+            double val = double.Parse(txtvalor.Text);
+            double total;
+
+            total = val * hor;
+
+            if (total < 700000)
+            {
+                total = (total * 0.10) + total;
+                txttotal.Text = total.ToString()  ;
+                MessageBox.Show("Incremento del salario del 10%" );
+            }
+
+            else
+            {
+                if (total <= 1000000)
+                {
+                    total = (total * 0.2) - total;
+                    txttotal.Text = total.ToString();
+                    MessageBox.Show("Descuento del salario del 2%");
+                }
+                else
+                {
+
+                    if (total >= 1000001)
+                    {
+                        total = (total / 0.5) * 0.2;
+                        txttotal.Text = total.ToString();
+                        MessageBox.Show("Descuento del salario del 5% y se le aumentó el 2%");
+                    }
+                }
+
+                
+            }
+
+        }
+    }
+}
